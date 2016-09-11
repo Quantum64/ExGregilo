242030bfb62b1f4f6818a299383a3ed916f77bef
Dylan <Quantum64Development@gmail.com>
2016-09-11 02:03:52 -0400

    Add a logger

--- src/main/java/co/q64/exgregilo/ExGregilo.java
+++ src/main/java/co/q64/exgregilo/ExGregilo.java
@@ -1,18 +1,38 @@
 package co.q64.exgregilo;
 
-import net.minecraft.init.Blocks;
+import co.q64.exgregilo.proxy.CommonProxy;
+import co.q64.exgregilo.util.SieveRegistryCleaner;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = ExGregilo.MODID, version = ExGregilo.VERSION)
 public class ExGregilo {
 	public static final String MODID = "exgregilo";
 	public static final String VERSION = "1.0";
 
+	@SidedProxy(clientSide = "co.q64.exgregilo.proxy.ClientProxy", serverSide = "co.q64.exgregilo.proxy.ServerProxy")
+	public static CommonProxy proxy;
+
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		proxy.setLogger(event.getModLog());
+	}
+
 	@EventHandler
 	public void init(FMLInitializationEvent event) {
-		// some example code
-		System.out.println("DIRT BLOCK >> " + Blocks.dirt.getUnlocalizedName());
+
+	}
+
+	@EventHandler
+	public void init(FMLPostInitializationEvent event) {
+		SieveRegistryCleaner.removeDefaultOres();
+	}
+
+	public static CommonProxy getProxy() {
+		return proxy;
 	}
 }
--- src/main/java/co/q64/exgregilo/proxy/CommonProxy.java
+++ src/main/java/co/q64/exgregilo/proxy/CommonProxy.java
@@ -1,5 +1,18 @@
 package co.q64.exgregilo.proxy;
 
-public abstract class CommonProxy {
+import org.apache.logging.log4j.Logger;
 
+public abstract class CommonProxy {
+	private Logger logger;
+
+	public void setLogger(Logger logger) {
+		if (getLogger() != null) {
+			getLogger().error("You cannot set the logger again!");
+		}
+		this.logger = logger;
+	}
+
+	public Logger getLogger() {
+		return logger;
+	}
 }
