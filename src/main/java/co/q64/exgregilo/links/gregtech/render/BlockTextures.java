72f417b8a8c01e6414c85a09abd760003fdb5266
Dylan <Quantum64Development@gmail.com>
2016-09-13 00:25:14 -0400

    Add a texture loader and a texture

---
+++ src/main/java/co/q64/exgregilo/links/gregtech/render/BlockTextures.java
@@ -0,0 +1,43 @@
+package co.q64.exgregilo.links.gregtech.render;
+
+import co.q64.exgregilo.data.ModData;
+import gregtech.api.GregTech_API;
+import gregtech.api.interfaces.IIconContainer;
+import net.minecraft.client.renderer.texture.TextureMap;
+import net.minecraft.util.IIcon;
+import net.minecraft.util.ResourceLocation;
+
+public enum BlockTextures implements IIconContainer, Runnable {
+	//formatter:off
+	OVERLAY_AUTO_SIEVE("OVERLAY_AUTO_SIEVE")
+	//formatter:on
+	;
+
+	private String textureName;
+	private IIcon icon;
+
+	private BlockTextures(String textureName) {
+		this.textureName = textureName;
+		GregTech_API.sGTBlockIconload.add(this);
+	}
+
+	@Override
+	public void run() {
+		icon = GregTech_API.sBlockIcons.registerIcon(ModData.ICON_TEX_PATH + textureName);
+	}
+
+	@Override
+	public IIcon getIcon() {
+		return icon;
+	}
+
+	@Override
+	public IIcon getOverlayIcon() {
+		return null;
+	}
+
+	@Override
+	public ResourceLocation getTextureFile() {
+		return TextureMap.locationBlocksTexture;
+	}
+}
