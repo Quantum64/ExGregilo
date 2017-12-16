5701e29d3a3153e49b202a6530241ed9a1a541e4
Dylan <Quantum64@users.noreply.github.com>
2017-12-15 23:56:51 -0500

    Create cleanup.js

---
+++ cleanup.js
@@ -0,0 +1,8 @@
+var githubRemoveAllReleases = require('github-remove-all-releases');
+
+var AUTH = {
+  type: 'oauth',
+  token: process.env.TOKEN
+};
+
+githubRemoveAllReleases(AUTH, 'stevemaotest', 'github-remove-all-releases-test', callback);
